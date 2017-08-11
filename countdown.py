e436c2b0d3630581003bf10b44f1fb3f76d0033b
Michael Fruen <mfruen@gmail.com>
2017-08-11 22:17:28 +0100

    bugfix: don't include invalid characters in length check

--- countdown.py
+++ countdown.py
@@ -15,16 +15,16 @@ class Solver:
         self._words = {word: Counter(word) for word in words}
 
     def get_matches(self, letters):
+        letters = letters.strip().lower()
+
         if not letters:
             return []
 
-        lcount = Counter(letters.strip().lower())
+        lcount = Counter(letters)
 
         for word, wcount in self._words.items():
             if len(word) > len(letters):
                 continue
 
-            if any(lcount[char] < count for char, count in wcount.items()):
-                continue
-
-            yield word
+            if all(lcount[char] >= count for char, count in wcount.items()):
+                yield word
